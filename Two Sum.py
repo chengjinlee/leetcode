f671928ff8d9f5b9e1b5a96c511866d4dc14ab07
juyue_zhang <harvey_lee@163.com>
2016-02-24 22:23:46 +0800

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#__author__=harvey_lee
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #计算nums的长度
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return i,j
+        return None
