1d4dc146fe23e576deff9ef19e4d93387b2a9acd
juyue_zhang <harvey_lee@163.com>
2016-03-09 23:19:42 +0800

    Create zigzag.py

---
+++ leetcode/6-ZigZag-Conversion/zigzag.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def convert(self, s, nRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if nRows==1: return s
+        tmp=['' for i in range(nRows)]
+        index=-1; step=1
+        for i in range(len(s)):
+            index+=step
+            if index==nRows:
+                index-=2
+                step=-1
+            elif index==-1:
+                index=1
+                step=1
+            tmp[index]+=s[i]
+        return ''.join(tmp)
